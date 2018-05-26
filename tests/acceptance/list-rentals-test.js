76499eb899d3f6073d80e86365b98c7e375a67dc
unknown <charles.kuo@gmail.com>
2018-05-26 00:33:04 -0700

    Add 7 failing, to be implemented tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,7 +5,8 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | list rentals', function(hooks) {
   // setupApplicationTest ensures app is started and shut down b/t each test
   setupApplicationTest(hooks);
-    // assert is an object passed in each test function by QUnit
+  /*  
+  // assert is an object passed in each test function by QUnit
     test('visiting /', async function(assert) {
       // Test helpers have functions like vist() and currentURL() 
       await visit('/');
@@ -13,4 +14,31 @@ module('Acceptance | list rentals', function(hooks) {
       assert.equal(currentURL(), '/');
       // A test must pass one assert to be successful 
     });
+  */
+
+  test('should show rentals as the home page.', async function (assert) {
+    // TODO
+  });
+
+  test('should link to information about the company.', async function(assert){
+    // TODO
+  });
+
+  test('should link to contact information.', async function(assert) {
+    // TODO
+  });
+
+  test('should list available rentals.', async function(assert) {
+    // TODO
+  });
+
+  test('should filter the list of rentals by city.', async function(assert) {
+    // TODO
+  });
+
+  test('should shoe details for a selected rental', async function(assert) {
+    // TODO
+  });
+
+
 });
