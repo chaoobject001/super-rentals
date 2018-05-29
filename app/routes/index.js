5724c210ea43e7d35b06f1e70ff4753da0c61431
unknown <charles.kuo@gmail.com>
2018-05-29 00:10:51 -0700

    Complete "Routes and Templates" with test

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // redirect user navigates to root (/) to /rentals
+  // with life cycle hook - beforeModel()
+  // beforeModel() hook is executed 
+  // before data fetched from model & before page rende
+  beforeModel() {
+    this.replaceWith('rentals'); // Replace current URL in browser history
+  }
 });
--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
@@ -17,15 +17,20 @@ module('Acceptance | list rentals', function(hooks) {
   */
 
   test('should show rentals as the home page.', async function (assert) {
-    // TODO
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
   test('should link to information about the company.', async function(assert){
-    // TODO
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function(assert) {
-    // TODO
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function(assert) {
